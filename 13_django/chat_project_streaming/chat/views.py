dc9845e0601bfc71e615f2c29ea5bf620574431e
YooSeonghyeon <ru20061@naver.com>
2026-01-23 12:24:41 +0900

    django2

--- 13_django/chat_project_streaming/chat/views.py
+++ 13_django/chat_project_streaming/chat/views.py
@@ -10,7 +10,6 @@ import traceback
 
 load_dotenv()
 
-
 def get_chain():
     """LangChain 체인을 초기화"""
 
@@ -30,19 +29,25 @@ def get_chain():
     chat = ChatOpenAI(model_name="gpt-5-mini")
     return prompt | chat
 
+# 챗봇 화면(html) 응답.
 def index(request):
     return render(request, 'chat/index.html')
 
+# 사용자 질의를 받아서 LLM에게 요청->응답을 streaming 으로 답.
 def stream_chat(request):
+    # 요청파라미터(GET방식)으로 사용자 질의를 받는다. "messages":"질의"
     message = request.GET.get('message', '')
     if not message:
         return StreamingHttpResponse("data: [ERROR] 메세지를 입력하세요.\n\n", content_type='text/event-stream')
-
+    
+    # Session 에서 대화히스토리(내역)을 저장. 없으면 만들어서(빈리스트) 
+    #   session에 저장.
     if 'message_history' not in request.session:
         request.session['message_history'] = []
-        request.session.modified = True
-        request.session.save()
+        request.session.modified = True # session상태가 변경됨을 표시
+        request.session.save() # 바뀐 상태를 저장/적용(보장)
 
+    # LLM에 요청을하고 streaming으로 응답을 받아서 제공하는 generator
     def event_stream():
         try:
             message_history = request.session.get('message_history', [])
@@ -62,15 +67,17 @@ def stream_chat(request):
             
             trimmed_msg = trim_messages(
                 message_history,
-                max_tokens=20,
-                strategy="last",
-                token_counter=len,
-                start_on="human",
-                end_on=("human", "ai"),
-                include_system=True
+                max_tokens=20, # 최대 20개 메세지만 유지
+                strategy="last", # 최근 것을 남기는 전략
+                token_counter=len, # max_tokens를 계산하는 방법 -len:메세지 개수,
+                start_on="human", # 메세지목록의 시작 ROLE
+                end_on=("human", "ai"), # 메세지 목록의 마지막 ROLE
+                include_system=True # System프롬프트를 유지할 지 여부.
             )
             
             message_history_to_save = []
+            # Session에 저장하기 위해서 HummanMessage/AIMessage 타입의 객체를
+            # dictionary(OpenAI chat 형식)으로 변환해서 저장.
             for msg in trimmed_msg:
                 if isinstance(msg, HumanMessage):
                     message_history_to_save.append({"role": "user", "content": msg.content})
@@ -89,7 +96,20 @@ def stream_chat(request):
             yield f"data: [ERROR] {str(e)}\n\n"
 
     response = StreamingHttpResponse(event_stream(), content_type='text/event-stream') # iterable
+    #  g=event_stream()
+    #  for token in g:
+    #     client에게 전송(token)
+
     response['Cache-Control'] = 'no-cache'
    
     return response
 
+
+# def gen():
+#     yield 1값
+
+#     yield 2값
+
+#     yield 3값
+
+# StreamingHttpResponse(gen, content_type='text/event-stream')
